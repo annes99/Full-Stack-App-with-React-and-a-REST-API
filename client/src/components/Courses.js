8ab2f7235788d9c2c88f8e26248fefaf9ebbfebb
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-06-22 22:43:08 +0700

    add courses+fetch

---
+++ client/src/components/Courses.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Courses extends Component {
+    constructor() {
+        super();
+        // keeping all fetched data states
+        this.state = {
+          courses: [],
+        };
+    }
+
+    componentDidMount() {
+    fetch('http://localhost:5000/api/courses')
+    .then(res => res.json())
+    .then((data) => {
+        this.setState({ courses: data.courses});
+    })
+    .catch(console.log);
+    }
+
+    render() {  
+        return (
+            <div className="bounds">
+                {this.state.courses.map((course) => 
+                <div className="grid-33" key={course.id}>
+                    <NavLink to={`/courses/${course.id}`} className="course--module course--link">
+                        <h4 className="course--label">Course</h4>
+                        <h3 className="course--title">{course.title}</h3>                            
+                    </NavLink> 
+                </div>
+                )}
+            
+                <div className="grid-33">
+                    <NavLink to='/courses/create' className="course--module course--add--module">
+                        <h3 className="course--add--title">
+                            <svg
+                                version="1.1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                x="0px"
+                                y="0px"
+                                viewBox="0 0 13 13"
+                                className="add">
+                                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
+                            </svg>New Course
+                        </h3>
+                    </NavLink> 
+                </div>
+            </div>
+
+        )
+    }
+
+}
+
+export default Courses;
