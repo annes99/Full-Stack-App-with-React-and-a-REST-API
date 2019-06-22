12299dcb380147d0d8bc8a893f3c6ee7c2ccf49c
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-06-22 22:42:14 +0700

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Provider } from './components/Context';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 import logo from './logo.svg';
-import './App.css';
+import './styles/global.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import Header from './components/Header';
+import Error from './components/Error';
+import Forbidden from './components/Forbidden';
+import UserSignOut from './components/UserSignOut';
+import NotFound from './components/NotFound';
+
+
+export default  class App extends Component {
+
+  render() {
+    return (
+      <Provider>
+      {/* <Provider value={this.state}> */}
+        <BrowserRouter>
+          <div>
+            <Header />
+              <Switch>
+                
+                <Route exact path="/" render={() => <Courses />}/>
+                <Route exact path="/courses/create" render={() => <CreateCourse  />} />
+                <Route exact path="/courses/:id/update" render={() => <UpdateCourse />} />
+                <Route exact path="/courses/:id" render={() => <CourseDetail />} />
+                <Route exact path="/signin" render={() => <UserSignIn />} />
+                <Route exact path="/signup" render={() => <UserSignUp />} />
+                <Route exact path="/signout" render={() => <UserSignOut />} />
+                <Route exact path="/forbidden" render={() => <Forbidden />} />
+
+                <Route component={NotFound} />
+
+              </Switch> 
+          </div>
+        </BrowserRouter>
+      </Provider>
+      
+    )
+  }
 }
-
-export default App;
