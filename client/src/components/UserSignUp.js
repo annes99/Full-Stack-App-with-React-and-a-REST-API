ac1e3e9935ff3c96bb160b7aa18ba7bd53c0fa7e
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-07-10 14:29:47 +0700

    add comments

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Consumer } from '../components/Context';
 import axios from 'axios';
 import { withRouter } from "react-router";
 
@@ -8,107 +7,127 @@ class UserSignUp extends Component {
 
     constructor() {
         super();
-        this.state = { 
+        this.state = {
             firstName: '',
             lastName: '',
             emailAddress: '',
             password: '',
-            confirmPassword: ''
+            confirmPassword: '',
+            error: ''
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-   
-    handleChange(e){
+   // Handles form input changes and updates this.state
+    handleChange(e) {
         this.setState({
             [e.target.name]: e.target.value
         });
     }
-
+    // Handles submitting the form. 
+    // POST request to api/users with current state values
+    // then triggers user log in and redirects user to api/courses page
+    // if there's error this.state error vill be updated
     handleSubmit(e) {
 
         e.preventDefault();
         const url = 'http://localhost:5000/api/users';
-    
-        axios.post(url, { 
+
+        axios.post(url, {
             firstName: this.state.firstName,
             lastName: this.state.lastName,
             emailAddress: this.state.emailAddress,
-            password: this.state.password 
+            password: this.state.password
 
         })
-          .then(res => {
-          console.log(res);
-          this.props.signIn(this.state.emailAddress, this.state.password);
-          this.props.history.push("/");
-    
-        })
-        .catch(function(error) {
-            console.log(error);
-          console.log('Error creating user');
-        });
+            .then(res => {
+                this.props.signIn(this.state.emailAddress, this.state.password);
+                this.props.history.push("/");
+
+            }, (error) => {
+                if (error) {
+                    // handle error 
+                    this.setState({
+                        error: error.response.data.errors
+                    });
+                }
+            });
     }
+    
 
     render() {
-        return (
-            <Consumer>
-            {value => (
-                <div className="bounds">
-                    <div className="grid-33 centered signin">
-                        <h1>Sign Up</h1>
-                        <div>
-                            <form onSubmit={this.handleSubmit}>
-                                <div><input 
-                                    id="firstName" 
-                                    name="firstName" 
-                                    type="text" 
-                                    placeholder="First Name"
-                                    onChange={this.handleChange}
-                                    autoFocus /></div>
-                                <div><input 
-                                    id="lastName" 
-                                    name="lastName" 
-                                    type="text"
-                                    placeholder="Last Name" 
-                                    onChange={this.handleChange}
-                                    /></div>
-                                <div><input 
-                                    id="emailAddress" 
-                                    name="emailAddress" 
-                                    type="text" 
-                                    placeholder="Email Address"
-                                    onChange={this.handleChange}
-                                    /></div>
-                                <div><input 
-                                    id="password" 
-                                    name="password" 
-                                    type="password" 
-                                    placeholder="Password"
-                                    onChange={this.handleChange}
-                                    /></div>
-                                <div><input 
-                                    id="confirmPassword" 
-                                    name="confirmPassword" 
-                                    type="password" 
-                                    placeholder="Confirm Password"
-                                    onChange={this.handleChange}
-                                    /></div>
-                                <div className="grid-100 pad-bottom"><button 
-                                    className="button"
-                                    type="submit">Sign Up</button>
-                                <NavLink to='/'><button className="button button-secondary">Cancel</button></NavLink></div>
-                            </form>
-                        </div>
-                        <p>&nbsp;</p>
-                        <p>Already have a user account? <NavLink to='/signin'>Click here</NavLink> to sign in!</p>
-                    </div>
-                </div>
 
-            )}
-            </Consumer>
+        return (
+            <div className="bounds">
+                <div className="grid-33 centered signin">
+                    <h1>Sign Up</h1>
+                    {/* checking if theres any errors, if so show them */}
+                    {this.state.error !== '' ? (
+                        <div>
+                            <h2 className="validation--errors--label">Validation errors</h2>
+                            <div className="validation-errors">
+                                <ul>
+                                    <li>{this.state.error[0]}</li>
+                                    <li>{this.state.error[1]}</li>
+                                    <li>{this.state.error[2]}</li>
+                                    <li>{this.state.error[3]}</li>
+                                    <li>{this.state.error[4]}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    ) : (
+                            null
+                        )
+                    }
+                    <div>
+                        <form onSubmit={this.handleSubmit}>
+                            <div><input
+                                id="firstName"
+                                name="firstName"
+                                type="text"
+                                placeholder="First Name"
+                                onChange={this.handleChange}
+                                autoFocus /></div>
+                            <div><input
+                                id="lastName"
+                                name="lastName"
+                                type="text"
+                                placeholder="Last Name"
+                                onChange={this.handleChange}
+                            /></div>
+                            <div><input
+                                id="emailAddress"
+                                name="emailAddress"
+                                type="text"
+                                placeholder="Email Address"
+                                onChange={this.handleChange}
+                            /></div>
+                            <div><input
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={this.handleChange}
+                            /></div>
+                            <div><input
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                type="password"
+                                placeholder="Confirm Password"
+                                onChange={this.handleChange}
+                            /></div>
+                            <div className="grid-100 pad-bottom"><button
+                                className="button"
+                                type="submit">Sign Up</button>
+                                <NavLink to='/'><button className="button button-secondary">Cancel</button></NavLink></div>
+                        </form>
+                    </div>
+                    <p>&nbsp;</p>
+                    <p>Already have a user account? <NavLink to='/signin'>Click here</NavLink> to sign in!</p>
+                </div>
+            </div>
         );
     }
-
 }
 
 export default withRouter(UserSignUp);
