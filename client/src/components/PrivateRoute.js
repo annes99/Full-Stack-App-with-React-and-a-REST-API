d3de2d3e025b84ceca1b5d3a8d90dcd457137407
Annes Silde <47981031+annes99@users.noreply.github.com>
2019-07-10 14:01:56 +0700

    add comments

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,33 +1,32 @@
-import React, { Component } from "react";
-import { NavLink, Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import { Consumer } from '../components/Context';
 
-
- const PrivateRoute = ({ component: Component, ...rest }) => {
+/* PrivateRoute checks if user is authenticated, if not redirects to /api/signin page 
+    https://reacttraining.com/react-router/web/example/auth-workflow
+*/
+const PrivateRoute = ({ component: Component, ...rest }) => {
     return (
         <Consumer>
             {value => (
-                //console.log(value.state.isAuthenticated)
                 <Route
                     {...rest}
                     render={props =>
                         value.state.isAuthenticated ? (
                             <Component {...props} />
                         ) : (
-                            <Redirect
-                            to={{
-                                pathname: "/signin",
-                                state: { from: props.location }
-                            }}
-                            />
-                        )
-                        
+                                <Redirect
+                                    to={{
+                                        pathname: "/signin",
+                                        state: { from: props.location }
+                                    }}
+                                />
+                            )
                     }
-                /> 
+                />
             )}
         </Consumer>
-
     );
-  }
+}
 
 export default PrivateRoute;
